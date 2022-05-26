a8d150babe2072de08dc5d1898759650fd144b6d
Mutakin <mutakin.email@gmail.com>
2022-05-27 06:06:52 +0700

    Create SplashScreenActivity.kt with theme

---
+++ app/src/main/java/com/capstone/valoai/SplashScreenActivity.kt
@@ -0,0 +1,15 @@
+package com.capstone.valoai
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+        finish()
+    }
+}
