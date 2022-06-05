aa91416c17ff7c1417b82c5a0879776d191e0a91
mutakin-san <mutakin.email@gmail.com>
2022-06-05 11:20:16 +0700

    display name and profile image from firebase to DashboardActivity.kt

--- app/src/main/java/com/capstone/valoai/features/dashboard/presentations/DashboardActivity.kt
+++ app/src/main/java/com/capstone/valoai/features/dashboard/presentations/DashboardActivity.kt
@@ -3,39 +3,51 @@ package com.capstone.valoai.features.dashboard.presentations
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.capstone.valoai.R
+import com.bumptech.glide.Glide
+import com.capstone.valoai.databinding.ActivityDashboardBinding
 import com.capstone.valoai.features.auth.presentation.login.LoginActivity
-import com.google.android.material.bottomappbar.BottomAppBar
+import com.capstone.valoai.features.maps.presentation.VaksinLocationMapsActivity
 import com.google.android.material.shape.MaterialShapeDrawable
 import com.google.android.material.shape.RelativeCornerSize
 import com.google.android.material.shape.RoundedCornerTreatment
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 
 class DashboardActivity : AppCompatActivity() {
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var binding: ActivityDashboardBinding
+    private var user: FirebaseUser? = null
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-    override fun onStart() {
-        super.onStart()
         firebaseAuth = FirebaseAuth.getInstance()
+        user = firebaseAuth.currentUser
 
         firebaseAuth.addAuthStateListener {
-            if(it.currentUser == null){
+            if (it.currentUser == null) {
                 startActivity(Intent(this@DashboardActivity, LoginActivity::class.java))
                 finish()
             }
         }
-    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
-        val bottomAppBar = findViewById<BottomAppBar>(R.id.bottomAppBar)
+        val bottomAppBar = binding.bottomAppBar
         val bottomBarBackground = bottomAppBar.background as MaterialShapeDrawable
         bottomBarBackground.shapeAppearanceModel = bottomBarBackground.shapeAppearanceModel
             .toBuilder()
             .setAllCorners(RoundedCornerTreatment()).setAllCornerSizes(RelativeCornerSize(0.1f))
             .build()
+
+        binding.txtName.text = user?.displayName
+        Glide.with(this@DashboardActivity).load(user?.photoUrl).into(binding.profileDashboard)
+        binding.profileDashboard.setOnClickListener {
+            firebaseAuth.signOut()
+        }
+        binding.fab.setOnClickListener {
+            startActivity(Intent(this@DashboardActivity, VaksinLocationMapsActivity::class.java))
+        }
     }
 }
