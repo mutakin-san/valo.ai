f07d267a3c651dbccf4a3e376005de5ddfaea7b7
Nur Syahfei <nur.syahfei@gmail.com>
2022-06-05 12:42:48 +0700

    list vaksin dashboard

--- app/src/main/java/com/capstone/valoai/features/dashboard/presentations/DashboardActivity.kt
+++ app/src/main/java/com/capstone/valoai/features/dashboard/presentations/DashboardActivity.kt
@@ -2,11 +2,14 @@ package com.capstone.valoai.features.dashboard.presentations
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.capstone.valoai.databinding.ActivityDashboardBinding
-import com.capstone.valoai.features.auth.presentation.login.LoginActivity
+import com.capstone.valoai.features.dashboard.domain.adapter.FikesListAdapter
 import com.capstone.valoai.features.maps.presentation.VaksinLocationMapsActivity
+import com.bumptech.glide.Glide
+import com.capstone.valoai.features.auth.presentation.login.LoginActivity
 import com.google.android.material.shape.MaterialShapeDrawable
 import com.google.android.material.shape.RelativeCornerSize
 import com.google.android.material.shape.RoundedCornerTreatment
@@ -15,14 +18,28 @@ import com.google.firebase.auth.FirebaseUser
 
 
 class DashboardActivity : AppCompatActivity() {
-    private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var binding: ActivityDashboardBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     private var user: FirebaseUser? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        this.title = "Dashboard"
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardBinding.inflate(layoutInflater)
+        attachFikesList()
+
         setContentView(binding.root)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        val bottomAppBar = binding.bottomAppBar
+        val bottomBarBackground = bottomAppBar.background as MaterialShapeDrawable
+        bottomBarBackground.shapeAppearanceModel = bottomBarBackground.shapeAppearanceModel
+            .toBuilder()
+            .setAllCorners(RoundedCornerTreatment()).setAllCornerSizes(RelativeCornerSize(0.1f))
+            .build()
 
         firebaseAuth = FirebaseAuth.getInstance()
         user = firebaseAuth.currentUser
@@ -34,12 +51,19 @@ class DashboardActivity : AppCompatActivity() {
             }
         }
 
-        val bottomAppBar = binding.bottomAppBar
-        val bottomBarBackground = bottomAppBar.background as MaterialShapeDrawable
-        bottomBarBackground.shapeAppearanceModel = bottomBarBackground.shapeAppearanceModel
-            .toBuilder()
-            .setAllCorners(RoundedCornerTreatment()).setAllCornerSizes(RelativeCornerSize(0.1f))
-            .build()
+    }
+
+    private fun attachFikesList() {
+        val dataDummy = arrayListOf("Test", "Test1")
+        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        binding.dashboardList.layoutManager = layoutManager
+        binding.dashboardList.adapter = FikesListAdapter(dataDummy)
+    }
+
+    fun onClickFeb(view: View) {
+        print("test")
+        startActivity(Intent(this@DashboardActivity, VaksinLocationMapsActivity::class.java))
 
         binding.txtName.text = user?.displayName
         Glide.with(this@DashboardActivity).load(user?.photoUrl).into(binding.profileDashboard)
